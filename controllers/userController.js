0d7492e91a0154a29bfe96d03a512ea7be21ef6f
Tony ID <tony.inuma@gmail.com>
2022-05-10 23:11:40 -0500

    Vista y controlador de users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,8 @@
 const userController = {};
 
-userController.index = (request, response) => {//es una funcion de flecha
+userController.index = (request, response) => {
 
-    //objeto
-    const usuario = {
-        "celular": "951423537",
-        "edad": 23,
-        "peso": 70,
-        "nombre": "luis",
-    }
-    usuario.direccion = "jr tablacacha";
-
-    response.render('home', { usuario: usuario }); //200 que esta bien  //500 error de la base de datos*/
-
+    response.render('users/index');
 }
 
 module.exports = userController; 
--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const homeController = require("../controllers/homeController");
+const userController = require("../controllers/userController");
 const router = express.Router()
 
 router.get("/", homeController.index);
-router.get("/users", homeController.index);
+router.get("/users", userController.index);
 router.get("/warehouse", homeController.index);
 
 module.exports = router; 
